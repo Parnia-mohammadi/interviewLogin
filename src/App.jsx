4c80ac0aa7d656885858f8669eb6992d57d0d57a
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-04-12 18:27:15 +0430

    header added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './components/Login/Login'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
+      <Header/>
       <Login />
     </div>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 function Header() {
   return (
-    <div>
-        <ul>
-            <li>
-                
-            </li>
-        </ul>
+    <div className="header_container">
+      <ul className="header_ul">
+        <li><a href="">Login and Logout</a></li>
+        <li><a href="">Users and todos</a></li>
+        <li><a href="">posts</a></li>
+        <li><a href="">search</a></li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
